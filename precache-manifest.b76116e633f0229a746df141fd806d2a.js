2b2e555cb0160150e5a4d778f3a71fd18e82032e
mstanka <mice78.mm@gmail.com>
2020-06-16 11:26:14 +0200

    Updates

---
+++ precache-manifest.b76116e633f0229a746df141fd806d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c791a9dd68308ff19e63d0b782ea8f8",
+    "url": "/ms-robofriends/index.html"
+  },
+  {
+    "revision": "13a2bb21304ac2b60468",
+    "url": "/ms-robofriends/static/css/main.0745293b.chunk.css"
+  },
+  {
+    "revision": "d3742a6afec76003b846",
+    "url": "/ms-robofriends/static/js/2.9aeee2ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ms-robofriends/static/js/2.9aeee2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a2bb21304ac2b60468",
+    "url": "/ms-robofriends/static/js/main.84877721.chunk.js"
+  },
+  {
+    "revision": "34c33408ebdbc53e9803",
+    "url": "/ms-robofriends/static/js/runtime-main.d5a4f19f.js"
+  }
+]);
